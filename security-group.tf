debed8ef280af58bfd6a8673b705daa36cf79974
greyhoundforty <ryantiffany@fastmail.com>
2020-06-08 12:24:10 -0500

    Removed un-needed variables
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- security-group.tf
+++ security-group.tf
@@ -30,58 +30,5 @@ resource "ibm_is_security_group_rule" "bastion_ssh_access_inbound" {
   }
 }
 
-resource "ibm_is_security_group_rule" "ingress_serf_wan_port_udp" {
-  depends_on = [ibm_is_security_group.consul_security_group]
-  group      = ibm_is_security_group.consul_security_group.id
-  direction  = "inbound"
-  remote     = "192.168.0.0/24"
-  udp {
-    port_min = var.serf_wan_port
-    port_max = var.serf_wan_port
-  }
-}
 
-resource "ibm_is_security_group_rule" "egress_serf_wan_port_udp" {
-  depends_on = [ibm_is_security_group.consul_security_group]
-  group      = ibm_is_security_group.consul_security_group.id
-  direction  = "outbound"
-  remote     = "192.168.0.0/24"
-  udp {
-    port_min = var.serf_wan_port
-    port_max = var.serf_wan_port
-  }
-}
-
-resource "ibm_is_security_group_rule" "egress_server_rpc_port_lan" {
-  depends_on = [ibm_is_security_group.consul_security_group]
-  group      = ibm_is_security_group.consul_security_group.id
-  direction  = "outbound"
-  remote     = "192.168.0.0/24"
-  tcp {
-    port_min = var.server_rpc_port
-    port_max = var.server_rpc_port
-  }
-}
-
-resource "ibm_is_security_group_rule" "ingress_server_rpc_port_lan" {
-  depends_on = [ibm_is_security_group.consul_security_group]
-  group      = ibm_is_security_group.consul_security_group.id
-  direction  = "inbound"
-  remote     = "192.168.0.0/24"
-  tcp {
-    port_min = var.server_rpc_port
-    port_max = var.server_rpc_port
-  }
-}
-
-resource "ibm_is_security_group_rule" "ssh_access" {
-  depends_on = [ibm_is_security_group.consul_security_group]
-  group      = ibm_is_security_group.consul_security_group.id
-  direction  = "inbound"
-  remote     = ibm_is_instance.bastion_instance.primary_network_interface[0].primary_ipv4_address
-  tcp {
-    port_min = "22"
-    port_max = "22"
-  }
-}
 
--- variables.tf
+++ variables.tf
@@ -34,38 +34,3 @@ variable "use_default_prefixes" {
   default     = "auto"
 }
 
-variable "server_rpc_port" {
-  description = "The port used by servers to handle incoming requests from other agents."
-  type        = number
-  default     = 8300
-}
-
-variable "cli_rpc_port" {
-  description = "The port used by all agents to handle RPC from the CLI."
-  type        = number
-  default     = 8400
-}
-
-variable "serf_lan_port" {
-  description = "The port used to handle gossip in the LAN. Required by all agents."
-  type        = number
-  default     = 8301
-}
-
-variable "serf_wan_port" {
-  description = "The port used by servers to gossip over the WAN to other servers."
-  type        = number
-  default     = 8302
-}
-
-variable "http_api_port" {
-  description = "The port used by clients to talk to the HTTP API"
-  type        = number
-  default     = 8500
-}
-
-variable "dns_port" {
-  description = "The port used to resolve DNS queries."
-  type        = number
-  default     = 8600
-}
