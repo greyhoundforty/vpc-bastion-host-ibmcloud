c49333f3aead6b9789d47feb5a6bba8dafd98d0a
greyhoundforty <ryantiffany@fastmail.com>
2020-06-08 16:22:59 -0500

    Updating to work on hyperion
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- README.md
+++ README.md
@@ -12,21 +12,22 @@ You can launch your cloud shell instance by clicking on the [Cloud Shell](https:
 Once your Cloud Shell instance spins up you will need to set your default region and VPC generation. This code example has only been tested with VPC Gen 2 architecture. 
 
 ```shell
-◲ ibmcloud is target us-south
-◲ ibmcloud is target --gen 2
+ibmcloud is target us-south
+ibmcloud is target --gen 2
 ```
 
 3. ### List images 
 With the region and generation set we can now list out the available images. 
 
 ```shell
-◲ ibmcloud is images --visibility public
+ibmcloud is images --visibility public
 
 ```
 Using the built-in `jq` utility we can filter by architecture and availability.
 
 ```shell
-◲ ibmcloud is images --visibility public --json | jq -r '.[] | select(.status=="available") | select(.operating_system.architecture=="amd64") | .name'
+ibmcloud is images --visibility public --json | jq -r '.[] | select(.status=="available") | select(.operating_system.architecture=="amd64") | .name'
+
 ibm-centos-7-6-minimal-amd64-2
 ibm-debian-9-12-minimal-amd64-1
 ibm-redhat-7-6-amd64-sap-applications-1
@@ -42,5 +43,5 @@ ibm-windows-server-2016-full-standard-amd64-3
 4. ### List Instance Profiles
 
 ```shell
-◲ ibmcloud is in-prs
+ibmcloud is in-prs
 ```
--- security-group.tf
+++ security-group.tf
@@ -29,4 +29,79 @@ resource "ibm_is_security_group" "vpc_secure_maintenance_sg" {
   vpc  = ibm_is_vpc.default_rt_vpc.id
 }
 
+resource "ibm_is_security_group_rule" "vpc_secure_maintenance_sg_tcp_dns_inbound" {
+  depends_on = [ibm_is_security_group.vpc_secure_maintenance_sg]
+  group      = ibm_is_security_group.vpc_secure_maintenance_sg.id
+  direction  = "outbound"
+  remote     = "0.0.0.0/0"
+  tcp {
+    port_min = "53"
+    port_max = "53"
+  }
+}
 
+resource "ibm_is_security_group_rule" "vpc_secure_maintenance_sg_udp_dns_outbound" {
+  depends_on = [ibm_is_security_group.vpc_secure_maintenance_sg]
+  group      = ibm_is_security_group.vpc_secure_maintenance_sg.id
+  direction  = "outbound"
+  remote     = "0.0.0.0/0"
+  udp {
+    port_min = "53"
+    port_max = "53"
+  }
+}
+
+resource "ibm_is_security_group_rule" "vpc_secure_maintenance_sg_tcp_http_outbound" {
+  depends_on = [ibm_is_security_group.vpc_secure_maintenance_sg]
+  group      = ibm_is_security_group.vpc_secure_maintenance_sg.id
+  direction  = "outbound"
+  remote     = "0.0.0.0/0"
+  tcp {
+    port_min = "80"
+    port_max = "80"
+  }
+}
+
+resource "ibm_is_security_group_rule" "vpc_secure_maintenance_sg_tcp_https_outbound" {
+  depends_on = [ibm_is_security_group.vpc_secure_maintenance_sg]
+  group      = ibm_is_security_group.vpc_secure_maintenance_sg.id
+  direction  = "outbound"
+  remote     = "0.0.0.0/0"
+  tcp {
+    port_min = "443"
+    port_max = "443"
+  }
+}
+
+resource "ibm_is_security_group_rule" "vpc_secure_maintenance_sg_tcp_https_outbound" {
+  depends_on = [ibm_is_security_group.vpc_secure_maintenance_sg]
+  group      = ibm_is_security_group.vpc_secure_maintenance_sg.id
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+  tcp {
+    port_min = "443"
+    port_max = "443"
+  }
+}
+
+resource "ibm_is_security_group_rule" "vpc_secure_maintenance_sg_ssh_inbound" {
+  depends_on = [ibm_is_security_group.vpc_secure_maintenance_sg]
+  group      = ibm_is_security_group.vpc_secure_maintenance_sg.id
+  direction  = "inbound"
+  remote     = ibm_is_security_group.vpc_secure_bastion_sg.id
+  tcp {
+    port_min = "22"
+    port_max = "22"
+  }
+}
+
+resource "ibm_is_security_group_rule" "vpc_secure_bastion_sg_ssh_outbound" {
+  depends_on = [ibm_is_security_group.vpc_secure_bastion_sg]
+  group      = ibm_is_security_group.vpc_secure_bastion_sg.id
+  direction  = "outbound"
+  remote     = ibm_is_security_group.vpc_secure_maintenance_sg.id
+  tcp {
+    port_min = "22"
+    port_max = "22"
+  }
+}
