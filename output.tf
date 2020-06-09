e315dd5fcc06eb449947554b9e64ff9bf19624f6
greyhoundforty <ryantiffany@fastmail.com>
2020-06-09 10:13:43 -0500

    Reset and redeploy
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- output.tf
+++ output.tf
@@ -5,3 +5,26 @@ output "bastion_floating_ip" {
 output "vpc_id" {
   value = ibm_is_vpc.default_rt_vpc.id
 }
+
+output "load_balancer_id" {
+  value = ibm_is_lb.web_lb.id
+}
+
+output "load_balancer_public_ips" {
+  value = ibm_is_lb.web_lb.public_ips
+}
+
+output "load_balancer_status" {
+  value = ibm_is_lb.web_lb.status
+}
+
+output "load_balancer_operating_status" {
+  value = ibm_is_lb.web_lb.operating_status
+}
+
+output "load_balancer_operating_hostname" {
+  value = ibm_is_lb.web_lb.hostname
+}
+
+
+
